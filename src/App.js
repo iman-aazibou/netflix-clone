7a5b6c6d4093908bc549da116306318a3eeb1393
iman-aazibou <imanaazibou@hotmail.com>
2022-02-07 13:06:42 +0100

    adding row components and banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Components/Row";
+import Banner from "./Components/Banner"
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner/>
+      <Row
+        title="NETFLIX ORIGINALS" isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action " fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
